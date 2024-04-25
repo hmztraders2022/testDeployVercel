e9c8a07c543ed54e4127a57568c379968f016b3d
hmztraders2022 <hamzah.tradersfamily@gmail.com>
2024-04-25 14:55:01 +0700

    git push

--- index.js
+++ index.js
@@ -14,6 +14,9 @@ app.get('/', (req, res) => {
 app.get('/about', (req, res) => {
   res.send('This is my about route..... ')
 })
+app.get('/x', (req, res) => {
+  res.send('This is XXXX')
+})
 
 // Export the Express API
 module.exports = app
